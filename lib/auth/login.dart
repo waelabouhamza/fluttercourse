7dbaf9e9705afb1a44afdc36135a6b47b0d3a0e0
waelabouhamza <97783930+waelabouhamza@users.noreply.github.com>
2023-07-22 14:31:35 +0300

    refresh

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -40,11 +40,106 @@ class _LoginState extends State<Login> {
               Text("Login To Continue Using The App",
                   style: TextStyle(color: Colors.grey)),
               Container(height: 20),
-              Text("Email"),
-              TextFormField(),
-              Text("Password"),
-              TextFormField(),
+              Text(
+                "Email",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(255, 184, 184, 184))),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(color: Colors.grey))),
+              ),
+              Container(height: 10),
+              Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Container(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Enter Your Password",
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(
+                            color: const Color.fromARGB(255, 184, 184, 184))),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                        borderSide: BorderSide(color: Colors.grey))),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 20),
+                alignment: Alignment.topRight,
+                child: Text(
+                  "Forgot Password ?",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ),
             ],
+          ),
+          MaterialButton(
+            height: 40,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            color: Colors.orange,
+            textColor: Colors.white,
+            onPressed: () {},
+            child: Text("Login"),
+          ),
+          Container(height: 20),
+
+          MaterialButton(
+              height: 40,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              color: Colors.red[700],
+              textColor: Colors.white,
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Login With Google  "),
+                  Image.asset(
+                    "images/4.png",
+                    width: 20,
+                  )
+                ],
+              )),
+          Container(height: 20),
+          // Text("Don't Have An Account ? Resister" , textAlign: TextAlign.center,)
+          InkWell(
+            onTap: (){
+              
+            },
+            child: Center(
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                  text: "Don't Have An Account ? ",
+                ),
+                TextSpan(
+                  text: "Register",
+                  style: TextStyle(color: Colors.orange , fontWeight: FontWeight.bold)
+                ),
+              ])),
+            ),
           )
         ]),
       ),
